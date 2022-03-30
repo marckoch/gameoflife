8abddcb43173a7070cb2fb84ad8b46d5866cab5d
Marc Koch <marckoch@users.noreply.github.com>
2022-03-30 18:38:06 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+group = "me.marckoch"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClassName = "MainKt"
+}
